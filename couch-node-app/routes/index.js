8399c6dcbe30922ff568184a3f404c6dd779aa43
Niklas Holmgren <a15nikho@student.his.se>
2018-03-08 12:42:17 +0100

    Lagt till dropdownmenyer för operationer
    insert fungerar fullständigt med dropdownmenyn, delete/update fungerar endas en i taget och select fungerar inte alls än så länge

--- couch-node-app/routes/index.js
+++ couch-node-app/routes/index.js
@@ -15,11 +15,34 @@ const couchAuth = new NodeCouchDb({
 });
 
 const dbname = 'testdb';
-const viewUrl = '_design/all_test/_view/all';
+const viewUrl = '_design/all_docs/_view/all';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'CouchDB', condition: true, anyArray: [1,2,3] }); //change title on index.html
+
+	var ids = [];    
+	var revs = [];
+	var resultArray = [];
+
+	couch.listDatabases().then(function(dbs){
+		couch.get(dbname, viewUrl).then(
+			function(data, headers,status){
+
+				for (var i in data.data.rows) {
+				  revs.push(data.data.rows[i].value.rev);
+				  ids.push(data.data.rows[i].id);
+				}
+				resultArray.push(data);
+			  	res.render('index', { title: 'CouchDB', condition: true, anyArray: [1,2,3], itemids: ids, itemrevs: revs, items:data.data.rows }); //change title on index.html
+			},
+			function(err){
+				res.send(err);
+			}
+		);
+	});
+
+
+	
 });
 
 
@@ -121,5 +144,114 @@ router.post('/delete/:id', function(req, res, next){
 		});
 });
 
+router.post('/dropdown', function(req, res, next){
+	var item = {
+		
+		antal: req.body.antal,
+		operation: req.body.operation,
+		format: req.body.format
+		
+	};
+	
+	
+	if(item.operation == "insert"){
+		console.log(item.antal + " insert was chosen");
+		for(var i = 0; i < item.antal; i++){
+			couch.insert('testdb', {name: item.operation, year: item.format, comment: item.antal}).then(
+			function(data, header, status){
+				
+			},
+			function(err){
+				res.send(err);
+			});
+		}
+		res.redirect('/');
+	}
+	else if(item.operation == "update"){
+		console.log(item.antal + " update was chosen");
+		var id = req.body.id;
+		var rev = req.body.rev;
+		console.log(req.body);
+	 	
+	
+			couch.update(dbname, {_id: id, _rev: rev, name: item.operation, year: item.format, comment: item.antal}).then(
+				function(data, header, status){
+					
+				},
+				function(err){
+					res.send(err);
+			});
+		
+		res.redirect('/');
+	}
+	else if(item.operation == "select"){
+		console.log("select was chosen");
 
+		var resultArray = [];
+		/*MongoClient.connect(url, function(err, client){
+			var db = client.db(dbName);
+			assert.equal(null, err);
+			
+			var cursor = db.collection('user').find().limit(parseInt(item.antal));
+
+			cursor.forEach(function(doc, err){
+				assert.equal(null, err);
+				resultArray.push(doc);
+			}, function(){
+				client.close();
+				res.render('index', {title: "Home", items: resultArray});
+			});
+
+		});*/
+
+		couch.listDatabases().then(function(dbs){
+		couch.get(dbname, viewUrl).then(
+			function(data, headers,status){
+				//console.log(data.data.rows);
+				res.render('index',{
+					items:data.data.rows, title: 'CouchDB'
+
+				});
+			},
+			function(err){
+				res.send(err);
+			}
+		);
+	});
+
+
+		res.redirect('/');
+	}
+	else if(item.operation == "delete"){
+		var id = req.body.id;
+		var rev = req.body.rev;
+
+		/*MongoClient.connect(url, function(err, client){
+			var db = client.db(dbName);
+	    	for(var i = 0; i < item.antal; i++){
+			    db.collection('user').deleteOne({"year": item.format}, function(error, result){
+					assert.equal(null, err);	
+				});
+		 	}
+		 	client.close();
+		}); 
+		*/
+		
+			couch.del(dbname, id, rev).then(
+				function(data, header, status){
+					
+				},
+				function(err){
+					res.send(err);
+			});
+	
+		//console.log(req);
+		console.log(item.antal + " delete was chosen");
+		res.redirect('/');
+	}
+	else{
+		console.log("something is broken!");
+	}
+	
+});
 module.exports = router;
